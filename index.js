5f714379a767276a27ad8eb71f2499115580f897
jonschlinkert <dev@sellside.com>
2014-10-06 00:03:46 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+/*!
+ * filter-object <https://github.com/jonschlinkert/filter-object>
+ *
+ * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Licensed under the MIT License
+ */
+
+'use strict';
+
+var makeIterator = require('make-iterator');
+var forOwn = require('for-own');
+
+module.exports = function filter(o, cb, thisArg) {
+  cb = makeIterator(cb, thisArg);
+  var filtered = {};
+
+  forOwn(o, function (value, key, o) {
+    if (cb(value, key, o)) {
+      filtered[key] = value;
+    }
+  });
+
+  return filtered;
+};
